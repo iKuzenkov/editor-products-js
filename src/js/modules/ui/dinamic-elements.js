1fdd0751ef94f6ba7414e96b67198538430d1445
Igor <greendaycom1@gmail.com>
2025-10-06 23:18:14 +0300

    upd: btn constructor

--- src/js/modules/ui/dinamic-elements.js
+++ src/js/modules/ui/dinamic-elements.js
@@ -1,15 +1,16 @@
-const createBtn = (place, element, elementClass, text) => {
+const createBtn = (place, element, elementClass, text, title) => {
   if (!place) {
     return console.error("Container not found");
   }
   const button = document.createElement(element);
   button.classList.add(elementClass);
   button.textContent = text;
+  button.title = title;
   place.append(button);
   return button;
 };
 
 export const createButton = () => {
   const container = document.querySelector("#container-sections-creation");
-  return createBtn(container, "button", "add-section", "Add Section");
+  return createBtn(container, "button", "add-section", "➕", "add section");
 };
