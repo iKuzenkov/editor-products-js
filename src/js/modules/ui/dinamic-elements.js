6525a6406d988ec9274ec2174f06807856e9f081
Igor <greendaycom1@gmail.com>
2025-10-07 23:36:55 +0300

    moved the logic

--- src/js/modules/ui/dinamic-elements.js
+++ src/js/modules/ui/dinamic-elements.js
@@ -1,14 +1,36 @@
 const container = document.querySelector("#container-sections-creation");
 
-const createBtn = (element, elementClass, text, title, place) => {
-  if (!place) {
+const createBtn = (element, elementClass, text, title, space) => {
+  if (!space) {
     return console.error("Container not found");
   }
   const el = document.createElement(element);
   el.classList.add(elementClass);
   el.textContent = text;
   el.title = title;
-  place.append(el);
+  space.append(el);
+  return el;
+};
+
+const createBlock = (element, elementClass, space) => {
+  if (!space) {
+    return console.error("Container not found");
+  }
+  const el = document.createElement(element);
+  el.classList.add(elementClass);
+  space.append(el);
+  return el;
+};
+
+const createInp = (element, elementClass, type, placeholder, space) => {
+  if (!space) {
+    return console.error("Div (section) not found");
+  }
+  const el = document.createElement(element);
+  el.classList.add(elementClass);
+  el.type = type;
+  el.placeholder = placeholder;
+  space.append(el);
   return el;
 };
 
@@ -16,31 +38,16 @@ export const createButton = () => {
   return createBtn("button", "btn-add-section", "➕", "add section", container);
 };
 
-const createBlock = (element, elementClass, place) => {
-  if (!place) {
-    return console.error("Container not found");
-  }
-  const el = document.createElement(element);
-  el.classList.add(elementClass);
-  place.append(el);
-  return el;
-};
-
 export const createDiv = () => {
-  return createBlock("div", "added-section", container);
-};
-
-const createInp = (element, elementClass, type, place) => {
-  if (!place) {
-    return console.error("Div (section) not found");
-  }
-  const el = document.createElement(element);
-  el.classList.add(elementClass);
-  el.type = type;
-  place.append(el);
-  return el;
+  return createBlock("div", "div-added-section", container);
 };
 
 export const createInput = (parent) => {
-  return createInp("input", "name-section", "text", parent);
+  return createInp(
+    "input",
+    "input-name-section",
+    "text",
+    "Enter section name",
+    parent
+  );
 };
