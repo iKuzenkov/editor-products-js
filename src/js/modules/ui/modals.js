093478e9e47f0496d8aca994f4d6d8d4fb966b8a
Igor <greendaycom1@gmail.com>
2025-10-25 01:33:51 +0300

    created modal window

--- src/js/modules/ui/modals.js
+++ src/js/modules/ui/modals.js
@@ -1,13 +1,45 @@
 import { createElement } from "../ui/dinamic-elements";
 
 export const settingProduct = (e) => {
-  // const product = e.target.closest(".inner-button-product");
-  // const dynamicDataAction = product.dataset.action;
-  // const modal = createDiv("div", "modal-hidden", dynamicDataAction, product);
-  // const modalContent = createDiv(
-  //   "div",
-  //   "modal-content",
-  //   dynamicDataAction,
-  //   modal
-  // );
+  const product = e.target.closest(".inner-button-product");
+  const dynamicDataAction = product.dataset.action;
+
+  const modal = createElement(
+    "div",
+    {
+      classList: ["modal"],
+      attrs: {
+        id: "modal",
+        "data-action": dynamicDataAction,
+      },
+    },
+    document.body
+  );
+  const overlay = createElement(
+    "div",
+    {
+      classList: ["modal-overlay"],
+    },
+    modal
+  );
+
+  const modalContent = createElement(
+    "div",
+    {
+      classList: ["modal-content"],
+    },
+    modal
+  );
+
+  const closeButton = createElement(
+    "button",
+    {
+      classList: ["close-modal"],
+      text: "close",
+    },
+    modalContent
+  );
+
+  overlay.addEventListener("click", () => modal.remove());
+  closeButton.addEventListener("click", () => modal.remove());
 };
