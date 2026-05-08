274319f2119855c399ac9e33ea87d790180f99f9
Igor <greendaycom1@gmail.com>
2026-05-09 00:01:03 +0300

    refactor: fething data via GitHub API

--- src/js/modules/ui/autofill-data/autofill.js
+++ src/js/modules/ui/autofill-data/autofill.js
@@ -3,13 +3,13 @@ import { api } from "../../interaction-helpers/api";
 import { dataInDOM } from "./get-data";
 
 /**
- * сalling a function that returns the received data from the passed address
+ * сolling a function that returns the received data from the passed address
  * @param {Event} e - button click
  * @returns {void}
  */
 export const getData = async (e) => {
   clearData();
 
-  const data = await api("https://jsonhosting.com/api/json/8299afe0/raw");
+  const data = await api("https://raw.githubusercontent.com/iKuzenkov/portfolio-api-data/refs/heads/main/product-editor/menu-mock-data.json");
   dataInDOM(data);
 };
